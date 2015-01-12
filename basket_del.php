af3cdb016f8f33300251fe3c918e093ecae35702
sdfdsf343dd <sdfdsf343dd@yandex.by>
2015-01-12 16:48:22 +0300

    del3

--- basket_del.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-$id=(int)$_GET['id'];
-setcookie($id, null);
-
-
-header('location:basket.php');
--- basket_edit.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-$id=(int)$_GET['id'];
-$val=(int)$_POST['tov'];
-setcookie($id, $val, time() + 60*60);
-
-
-header('location:basket.php');
